c65550ba1905fa23f5c93553a443f7fc519d33b6
Adam Spittler & Henri El Daher <pair+adamspittler.heldaher@devbootcamp.com>
2016-08-31 12:27:02 -0400

    Add login and register

---
+++ app/controllers/user.rb
@@ -0,0 +1,44 @@
+get '/users' do
+
+  @users = User.all #define instance variable for view
+
+  erb :'users/index' #show all users view (index)
+
+end
+
+get '/users/new' do
+
+  erb :'users/new' #show new users view
+
+end
+
+post '/users' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @errors = user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+get '/users/login' do
+  erb :'users/login'
+end
+
+post '/users/login' do
+  @user = User.find_by(username: params[:username])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = "Incorrect username and password combination"
+    erb :"users/login"
+  end
+end
+
+
+
+
+
