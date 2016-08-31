1aaebcf68cdb00776a785a1c5085d2473a3c0d01
Henri El Daher & Richie Ito <pair+heldaher.ricito4@devbootcamp.com>
2016-08-31 18:30:16 -0400

    did comment things but this is the wrong branch for it lol

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,9 +1,33 @@
 get '/questions/:id/comment/new' do
+  require_user
   @question= Question.find(params[:id])
-  erb :'/questions/comment'
+  erb :'/questions/_question_comment_form'
 end
 
 get '/answers/:id/comment/new' do
+  require_user
   @answer= Answer.find(params[:id])
-  erb :'/answers/comment'
+  erb :'/answers/_answer_comment_form'
+end
+
+post '/questions/:id/comment' do
+  @comment= Comment.new(params[:comment])
+  if @comment.save
+    redirect "/questions/#{params[:id]}"
+  else
+    @errors= @comment.errors.full_messages
+    erb :'/questions/_question_comment_form'
+  end
+end
+
+post '/answers/:id/comment' do
+  @comment= Comment.new(params[:comment])
+  question_id= Answer.find(params[:id]).question.id
+
+  if @comment.save
+    redirect "/questions/#{question_id}"
+  else
+    @errors= @comment.errors.full_messages
+    erb :'/answers/_answer_comment_form'
+  end
 end
