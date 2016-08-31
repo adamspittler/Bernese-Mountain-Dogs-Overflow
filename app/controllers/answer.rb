47a67b4e811c44b1ec39bb153fa1cbd16bf4e527
Henri El Daher & Richie Ito <pair+heldaher.ricito4@devbootcamp.com>
2016-08-31 16:02:33 -0400

    can create answers

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,3 +1,14 @@
-get '/questions/:id/answers/new' do
+get '/questions/:id/answer/new' do
+  @question= Question.find(params[:id])
   erb :'/answers/create'
+end
+
+post '/answers' do
+  answer=Answer.new(params[:answer])
+  if answer.save
+    redirect '/'
+  else
+    @error=answer.errors.full_messages
+    erb :'/answers/create'
+  end
 end
