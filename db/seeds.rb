3beda03f38f233ccfd147a7b6267a99f4ab31e74
Henri El Daher & Richie Ito <pair+heldaher.ricito4@devbootcamp.com>
2016-08-31 10:00:36 -0400

    initial setup

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+users= [ {username: "Rubiksdude", email: "rubiks@dude.wow", password: "123456" },{username: "Henri", email: "henri@gmail.com", password: "henri"}, {username: "fluffy", email: "fluffy@email.com", password: "fluffy"}
+]
+
+User.create!(users)
